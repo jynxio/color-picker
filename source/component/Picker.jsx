b2cf7f2d9b7934d81297091d484a8c471cc209e8
jynxio <1337816495@qq.com>
2023-01-02 02:19:15 +0800

    update

--- source/component/Picker.jsx
+++ source/component/Picker.jsx
@@ -25,6 +25,7 @@ function RgbPicker () {
             <RgbRibbon name={ "green" } value={ getGreen() } setValue={ value => setGreen( value ) } />
             <RgbRibbon name={ "blue" } value={ getBlue() } setValue={ value => setBlue( value ) } />
             <RgbRibbon name={ "alpha" } value={ getAlpha() } setValue={ value => setAlpha( value ) } />
+            <RgbOutput red={ getRed() } green={ getGreen() } blue={ getBlue() } alpha={ getAlpha() } />
         </div>
     );
 
@@ -112,6 +113,29 @@ function RgbRibbon ( props ) {
 
 function RgbOutput ( props ) {
 
+    return (
+        <div class={ style.output }>
+            <span class={ style.content }>
+                { calculateHex( props.red, props.green, props.blue, props.alpha ) }
+            </span>
+            <span class={ style.toggle }>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="17 11 21 7 17 3"></polyline><line x1="21" y1="7" x2="9" y2="7"></line><polyline points="7 21 3 17 7 13"></polyline><line x1="15" y1="17" x2="3" y2="17"></line></svg>
+            </span>
+            <span class={ style.copy }>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+            </span>
+        </div>
+    );
 
+    function calculateHex ( r, g, b, a ) {
+
+        const hex_r = r.toString( 16 );
+        const hex_g = g.toString( 16 );
+        const hex_b = b.toString( 16 );
+        const hex_a = a.toString( 16 );
+
+        return "#" + hex_r + hex_g + hex_b + hex_a;
+
+    }
 
 }
