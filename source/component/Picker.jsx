a3be9557fd7cb3d96abd082e29ebb3bffe6c6b1d
jynxio <1337816495@qq.com>
2023-01-01 21:07:20 +0800

    update

--- source/component/Picker.jsx
+++ source/component/Picker.jsx
@@ -1,6 +1,10 @@
 import style from "./picker.module.css";
 
-import { createSignal, onMount, For, onCleanup } from "solid-js";
+import { For } from "solid-js";
+
+import { createSignal, createEffect } from "solid-js";
+
+import { onMount, onCleanup } from "solid-js";
 
 export default function () {
 
@@ -10,103 +14,128 @@ export default function () {
 
 function RgbPicker () {
 
-    const names = [ "red", "green", "blue", "alpha" ];
+    let red_anchor_dom;
+    let red_anchor_dom_width;
 
-    return (
-        <div class={ style.picker }>
-            <For each={ names }>{ name => <RgbColorMap name={ name } /> }</For>
-        </div>
-    );
+    const color_names = [ "red", "green", "blue", "alpha" ];
 
-}
+    const [ getInitialX, setInitialX ] = createSignal( NaN );
+    const [ getCurrentX, setCurrentX ] = createSignal( NaN );
 
-/**
- * RGB色带构造器。
- * @param { Object } props - 参数字典。
- * @param { string } props.name - 颜色名，仅限于"red"、"green"、"blue"、"alpha"其中的一个。
- * @returns { JSX } - 色带。
- */
-function RgbColorMap ( props ) {
+    const [ getInitialValue, setInitialValue ] = createSignal( NaN ); // TODO 这是个糟糕的设计！你应该用更简洁的方法来实现它！
 
-    let is_down = false;
-    let previous_color_value = 128;
-    let previous_x, anchor_dom, anchor_dom_width;
-
-    const resize_observer = new ResizeObserver( entries => {
-
-        anchor_dom_width = entries[ 0 ].contentBoxSize[ 0 ].inlineSize;
-
+    const [ getCapturedAnchorName, setCapturedAnchorName ] = createSignal( "" );
+    const [ getColorTable, setColorTable ] = createSignal( {
+        [ color_names[ 0 ] ]: 128,
+        [ color_names[ 1 ] ]: 128,
+        [ color_names[ 2 ] ]: 128,
+        [ color_names[ 3 ] ]: 128,
     } );
 
-    onMount( _ => {
+    const observer = new ResizeObserver( entries => red_anchor_dom_width = entries[ 0 ].contentBoxSize[ 0 ].inlineSize );
 
-        // TODO 将这两个事件监听器提升到RgbPicker中去，以减少事件监听器的数量。
+    onMount( _ => {
 
         globalThis.addEventListener( "pointerup", handlePointerUpEvent );
         globalThis.addEventListener( "pointermove", handlePointerMoveEvent );
 
-        resize_observer.observe( anchor_dom );
+        observer.observe( red_anchor_dom );
 
     } );
 
-    onCleanup(  _ => {
+    onCleanup( _ => {
 
         globalThis.removeEventListener( "pointerup", handlePointerUpEvent );
         globalThis.removeEventListener( "pointermove", handlePointerMoveEvent );
 
-        resize_observer.unobserve( anchor_dom );
+        observer.unobserve( red_anchor_dom );
 
     } );
 
-    const [ getColorValue, setColorValue ] = createSignal( previous_color_value );
-
     return (
-        <div class={ style[ props.name ] }>
-            <span class={ style[ "color-name" ] }>
-                { props.name[ 0 ].toUpperCase().concat( ... props.name.slice( 1 ) ) }
-            </span>
-            <span class={ style[ "color-value" ] }>
-                { getColorValue() }
-            </span>
-            <div class={ style[ "color-map" ] }>
-                <div class={ style[ "overlay" ] }></div>
-                <div class={ style[ "anchor" ] } ref={ anchor_dom }>
-                    <span
-                        onPointerDown={ handlePointerDownEvent }
-                        style={ { left: getColorValue() / 255 * 100 + "%" } }
-                    ></span>
-                </div>
-            </div>
+        <div class={ style.picker }>
+            <For each={ color_names }>
+                {
+                    color_name => {
+
+                        const text = color_name[ 0 ].toUpperCase().concat( ... color_name.slice( 1 ) );
+
+                        return (
+                            <div class={ style[ color_name ] }>
+                                <span class={ style[ "color-text" ] }>{ text }</span>
+                                <span class={ style[ "color-value" ] }>{ getColorTable()[ color_name ] }</span>
+                                <div class={ style[ "color-map" ] }>
+                                    <div class={ style[ "overlay" ] }></div>
+                                    <div
+                                        class={ style[ "anchor" ] }
+                                        onPointerDown={ handlePointerDownEvent }
+                                        ref={ ref => color_name === color_names[ 0 ] && ( red_anchor_dom = ref ) }
+                                    >
+                                        <span style={ { left: getColorTable()[ color_name ] / 255 * 100 + "%" } }></span>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+
+                        // TODO 如何注销这个事件呢？
+                        function handlePointerDownEvent ( event ) {
+
+                            setInitialX( event.screenX );
+                            setCurrentX( event.screenX );
+
+                            setInitialValue( getColorTable()[ color_name ] );
+
+                            setCapturedAnchorName( color_name );
+
+                        }
+
+                    }
+                }
+            </For>
         </div>
     );
 
-    function handlePointerUpEvent ( event ) {
+    function handlePointerUpEvent () {
 
-        if ( is_down ) is_down = false;
+        setInitialX( NaN );
+        setCurrentX( NaN );
+
+        setInitialValue( NaN );
+
+        setCapturedAnchorName( "" );
 
     }
 
     function handlePointerMoveEvent ( event ) {
 
-        if ( ! is_down ) return;
+        if ( getCapturedAnchorName() === "" ) return;
 
-        let next_color_value = ( event.screenX - previous_x ) / anchor_dom_width * 255 + previous_color_value;
+        /**
+         *
+         */
+        if ( Number.isNaN( getInitialX() ) ) throw new Error( "Error: An unexpected situation." );
+        if ( Number.isNaN( getCurrentX() ) ) throw new Error( "Error: An unexpected situation." );
 
-        next_color_value = Math.round( next_color_value );
+        setCurrentX( event.screenX );
 
-        if ( next_color_value < 0 ) next_color_value = 0;
-        if ( next_color_value > 255 ) next_color_value = 255;
+        const initial_x = getInitialX();
+        const current_x = getCurrentX();
 
-        setColorValue( next_color_value );
+        const offset_x = current_x - initial_x;
 
-    }
+        /**
+         *
+         */
+        const color_table = getColorTable();
+        const color_name = getCapturedAnchorName();
 
-    function handlePointerDownEvent ( event ) {
+        let initial_color_value = getInitialValue();
+        let current_color_value = Math.round( offset_x / red_anchor_dom_width * 255 + initial_color_value );
 
-        is_down = true;
+        current_color_value = Math.max( current_color_value, 0 );
+        current_color_value = Math.min( current_color_value, 255 );
 
-        previous_x = event.screenX;
-        previous_color_value = getColorValue();
+        setColorTable( { ... color_table, [ color_name ]: current_color_value } )
 
     }
 
