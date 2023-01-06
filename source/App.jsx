5451e5c8475df1dfa3edd753cab2978650bfda4b
jynxio <1337816495@qq.com>
2023-01-07 03:09:30 +0800

    update

--- source/App.jsx
+++ source/App.jsx
@@ -6,7 +6,7 @@ import style from "./app.module.css";
 /**
  *
  */
-import { createSignal, useContext } from "solid-js";
+import { createSignal, onCleanup, onMount, useContext } from "solid-js";
 
 import { GlobalColorContext } from "./context/GlobalColorContext";
 
@@ -23,19 +23,35 @@ function App () {
 
     const global_color = useContext( GlobalColorContext );
 
-    const [ getMode, setMode ] = createSignal( "hex" ); // "hex" or "rgb" or "hsl"
+    const formats = [ "hex", "rgb", "hsl" ];
+    const [ getFormat, setFormat ] = createSignal( formats[ 0 ] );
+
+    onMount( _ => globalThis.addEventListener( "keydown", handleKeyDownEvent ) );
+    onCleanup( _ => globalThis.removeEventListener( "keydown", handleKeyDownEvent ) );
 
     return (
-        <GlobalColorContext.Provider>
-            <div
-                class={ style.container }
-                style={ { "background-color": createRgbString( global_color.getRgb() ) } }
-            >
-                <Palette mode={ getMode() }/>
-                {/* <Output getMode={ getMode } setMode={ setMode }/> */}
-            </div>
-        </GlobalColorContext.Provider>
+        <div
+            class={ style.container }
+            style={ { "background-color": createRgbString( global_color.getRgb() ) } }
+        >
+            <Palette format={ getFormat() }/>
+            {/* <Output getMode={ getMode } setMode={ setMode }/> */}
+        </div>
     );
+
+    function handleKeyDownEvent ( event ) {
+
+        if ( event.key.toLowerCase() !== "shift" ) return;
+
+        let next_index = formats.indexOf( getFormat() ) + 1;
+
+        if ( next_index === formats.length ) next_index = 0;
+
+        const next_format = formats[ next_index ];
+
+        setFormat( next_format );
+
+    }
 
 }
 
