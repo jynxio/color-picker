3a577e88a6d2bcac5ce6fe6434e93d1fa5663d5b
jynxio <1337816495@qq.com>
2023-01-14 03:15:11 +0800

    Update Cartesian.jsx

--- source/component/component/Cartesian.jsx
+++ source/component/component/Cartesian.jsx
@@ -3,6 +3,7 @@ import style from "./cartesian.module.css";
 
 /* -------------------------------------------------------------------------------------------- */
 import { createEffect, createMemo, createSignal, onCleanup, onMount } from "solid-js";
+import { equal } from "../../math/equal";
 
 /**
  * 色板组件构造器。
@@ -18,15 +19,20 @@ function Cartesian ( props ) {
     const initial_memo_values = props.getValues();
     const getMemoValues = createMemo( _ => props.getValues(), initial_memo_values, { equals: ( prev, next ) => {
 
-        for ( let i = 0; i < prev.length; i ++ ) {
+        if ( ! equal( prev[ 0 ], next[ 0 ] ) ) return false;
+        if ( ! equal( prev[ 1 ], next[ 1 ] ) ) return false;
 
-            if ( prev[ i ] !== next[ i ] ) return true
-
-        }
-
-        return false;
+        return true;
 
     } } );
+
+    let dom;
+    let size;
+
+    let base_values;
+    let base_positions;
+
+    const observer = new ResizeObserver( entries => size = entries[ 0 ].contentBoxSize[ 0 ].inlineSize );
 
     createEffect( _ => {
 
@@ -40,12 +46,16 @@ function Cartesian ( props ) {
 
     onMount( _ => {
 
+        observer.observe( dom );
+
         globalThis.addEventListener( "pointerup", handlePointerUpEvent, false );
         globalThis.addEventListener( "pointermove", handlePointerMoveEvent, false );
 
     } );
 
     onCleanup( _ => {
+
+        observer.unobserve( dom );
 
         globalThis.removeEventListener( "pointerup", handlePointerUpEvent, false );
         globalThis.removeEventListener( "pointermove", handlePointerMoveEvent, false );
@@ -56,13 +66,19 @@ function Cartesian ( props ) {
 
     return (
         <div class={ style.cartesian }>
-            <div class={ `${ style.axis } ${ style.x }` }>
+            <div class={ `${ style.axis } ${ style.horizontal } ${ style.tag }` }>
                 <span>{ props.names[ 0 ] }</span>
             </div>
-            <div class={ `${ style.axis } ${ style.y }` }>
+            <div class={ `${ style.axis } ${ style.horizontal } ${ style.value }` }>
+                <span>{ getMemoValues()[ 0 ] + "%" }</span>
+            </div>
+            <div class={ `${ style.axis } ${ style.vertical } ${ style.tag }` }>
                 <span>{ props.names[ 1 ] }</span>
             </div>
-            <div class={ style.board }>
+            <div class={ `${ style.axis } ${ style.vertical } ${ style.value }` }>
+                <span>{ getMemoValues()[ 1 ] + "%" }</span>
+            </div>
+            <div class={ style.board } ref={ dom }>
                 <div class={ `${ style.mixcolor } ${ style.lower }` }></div>
                 <div class={ `${ style.mixcolor } ${ style.upper }` }></div>
                 <span class={ style.anchor } style={ createStyle() } onPointerDown={ handlePointerDownEvent }></span>
@@ -72,11 +88,11 @@ function Cartesian ( props ) {
 
     function createStyle () {
 
-        // TODO 计算top和left，我不想让这个计算过程与CSS的--border-width耦合在一起，我需要重构吗？
-
+        const top = getMemoValues()[ 1 ] + "%";
+        const left = getMemoValues()[ 0 ] + "%";
         const cursor = getEnabled() ? "all-scroll" : "grab";
 
-        return { cursor };
+        return { top, left, cursor };
 
     }
 
@@ -90,9 +106,31 @@ function Cartesian ( props ) {
 
         setEnabled( true );
 
+        base_values = getMemoValues();
+        base_positions = [ event.screenX, event.screenY ];
+
     }
 
-    function handlePointerMoveEvent ( event ) {}
+    function handlePointerMoveEvent ( event ) {
+
+        if ( ! getEnabled() ) return;
+
+        const next_values = [];
+        const next_positions = [ event.screenX, event.screenY ];
+
+        next_values[ 0 ] = ( next_positions[ 0 ] - base_positions[ 0 ] ) / size * 100 + base_values[ 0 ];
+        next_values[ 0 ] = Math.min( next_values[ 0 ], 100 );
+        next_values[ 0 ] = Math.max( next_values[ 0 ], 0 );
+        next_values[ 0 ] = Math.round( next_values[ 0 ] );
+
+        next_values[ 1 ] = ( next_positions[ 1 ] - base_positions[ 1 ] ) / size * 100 + base_values[ 1 ];
+        next_values[ 1 ] = Math.min( next_values[ 1 ], 100 );
+        next_values[ 1 ] = Math.max( next_values[ 1 ], 0 );
+        next_values[ 1 ] = Math.round( next_values[ 1 ] );
+
+        props.setValues( next_values );
+
+    }
 
 }
 
