1e709a30e75fa0e93c4b401cb9f450d42f988d57
jynxio <1337816495@qq.com>
2023-01-17 00:13:00 +0800

    Update App.jsx

--- source/App.jsx
+++ source/App.jsx
@@ -5,12 +5,13 @@ import { createSignal, onCleanup, onMount } from "solid-js";
 import { getGlobalRgb } from "./color/color";
 import { Palette } from "./component/Palette";
 import { Output } from "./component/Output";
+import { Footer } from "./component/Footer";
 
 /* -------------------------------------------------------------------------------------------- */
 function App () {
 
-    const [ getOutputFormat, setOutputFormat ] = createSignal( "rgb" );   // "rgb" or "hex" or "hsl"
-    const [ getPaletteFormat, setPaletteFormat ] = createSignal( "rgb" ); // "rgb" or "hsl"
+    const [ getOutputFormat, setOutputFormat ] = createSignal( "hsl" );   // "rgb" or "hex" or "hsl"
+    const [ getPaletteFormat, setPaletteFormat ] = createSignal( "hsl" ); // "rgb" or "hsl"
 
     onMount( _ => globalThis.addEventListener( "keydown", handleKeyDownEvent ) );
     onCleanup( _ => globalThis.removeEventListener( "keydown", handleKeyDownEvent ) );
@@ -27,6 +28,7 @@ function App () {
                 toNextOutputFormat={ toNextOutputFormat }
                 toNextPaletteFormat={ toNextPaletteFormat }
             />
+            <Footer/>
         </div>
     );
 
