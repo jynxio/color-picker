d703138028fdb59b63c597cc421f9a961e9db386
jynxio <1337816495@qq.com>
2023-01-22 23:44:12 +0800

    finish button's animation

--- source/component/Output.jsx
+++ source/component/Output.jsx
@@ -2,7 +2,7 @@
 import style from "./output.module.css";
 
 /* -------------------------------------------------------------------------------------------- */
-import { createMemo, onCleanup, onMount } from "solid-js";
+import { createSignal, createMemo, onCleanup, onMount } from "solid-js";
 import { getGlobalRgb, getGlobalHex, getGlobalHsl } from "../color/color";
 import { rgbToString, hexToString, hslToString } from "../color/convertor";
 import { easeOutExpo } from "../math/easing";
@@ -20,14 +20,13 @@ import { Tween } from "../math/tween";
  */
 function Output ( props ) {
 
-    const copy_tween = createTween(  ) // BUG
-
     let copy_button;
     let output_button;
     let palette_button;
 
-    onMount( _ => globalThis.addEventListener( "keydown", handleKeyDownEvent ) );
-    onCleanup( _ => globalThis.removeEventListener( "keydown", handleKeyDownEvent ) );
+    const [ getCopyStyle, animateCopyStyle ] = createAnimatedStyle();
+    const [ getOutputStyle, animateOutputStyle ] = createAnimatedStyle();
+    const [ getPaletteStyle, animatePaletteStyle ] = createAnimatedStyle();
 
     const getBorderColor = createMemo( _ => {
 
@@ -40,34 +39,37 @@ function Output ( props ) {
 
     } );
 
+    onMount( _ => globalThis.addEventListener( "keydown", handleKeyDownEvent ) );
+    onCleanup( _ => globalThis.removeEventListener( "keydown", handleKeyDownEvent ) );
+
     return (
         <div class={ style.output }>
             <pre class={ style.value } style={ { "border-color": getBorderColor() } }>{ createColorString() }</pre>
             <div class={ style.controller } style={ { "border-color": getBorderColor() } }>
-                <button ref={ copy_button } onPointerDown={ handleCopyEvent }><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg></button>
-                <button ref={ output_button } onPointerDown={ handleSwitchOutputFormatEvent }><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18.37 2.63 14 7l-1.59-1.59a2 2 0 0 0-2.82 0L8 7l9 9 1.59-1.59a2 2 0 0 0 0-2.82L17 10l4.37-4.37a2.12 2.12 0 1 0-3-3Z"></path><path d="M9 8c-2 3-4 3.5-7 4l8 10c2-1 6-5 6-7"></path><path d="M14.5 17.5 4.5 15"></path></svg></button>
-                <button ref={ palette_button } onPointerDown={ handleSwitchPaletteFormatEvent }><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"></circle><circle cx="17.5" cy="10.5" r=".5"></circle><circle cx="8.5" cy="7.5" r=".5"></circle><circle cx="6.5" cy="12.5" r=".5"></circle><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"></path></svg></button>
+                <button
+                    ref={ copy_button }
+                    style={ getCopyStyle() }
+                    onPointerDown={ handleCopyEvent }
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+                </button>
+                <button
+                    ref={ output_button }
+                    style={ getOutputStyle() }
+                    onPointerDown={ handleSwitchOutputFormatEvent }
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18.37 2.63 14 7l-1.59-1.59a2 2 0 0 0-2.82 0L8 7l9 9 1.59-1.59a2 2 0 0 0 0-2.82L17 10l4.37-4.37a2.12 2.12 0 1 0-3-3Z"></path><path d="M9 8c-2 3-4 3.5-7 4l8 10c2-1 6-5 6-7"></path><path d="M14.5 17.5 4.5 15"></path></svg>
+                </button>
+                <button
+                    ref={ palette_button }
+                    style={ getPaletteStyle() }
+                    onPointerDown={ handleSwitchPaletteFormatEvent }
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"></circle><circle cx="17.5" cy="10.5" r=".5"></circle><circle cx="8.5" cy="7.5" r=".5"></circle><circle cx="6.5" cy="12.5" r=".5"></circle><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"></path></svg>
+                </button>
             </div>
         </div>
     );
-
-    function createTween ( dom ) {
-
-        return new Tween()
-            .fromTo( 0, 255 )
-            .setDuration( 2000 )
-            .setEasing( easeOutExpo )
-            .setListener( strength => {
-
-                const font_color = strength;
-                const background_color = 255 - strength;
-
-                dom.style.setProperty( "color", `rgb( ${ font_color } ${ font_color } ${ font_color } / 1 )` );
-                dom.style.setProperty( "background-color", `rgb( ${ background_color } ${ background_color } ${ background_color } / 1 )` )
-
-            } );
-
-    }
 
     function createColorString () {
 
@@ -86,7 +88,7 @@ function Output ( props ) {
 
     function handleCopyEvent () {
 
-        animate( tween );
+        animateCopyStyle();
 
         const format = props.outputFormat; // "rgb" or "hex" or "hsl"
         const text =
@@ -103,11 +105,15 @@ function Output ( props ) {
 
     function handleSwitchOutputFormatEvent () {
 
+        animateOutputStyle();
+
         props.toNextOutputFormat();
 
     }
 
     function handleSwitchPaletteFormatEvent () {
+
+        animatePaletteStyle();
 
         props.toNextPaletteFormat();
 
@@ -122,6 +128,33 @@ function Output ( props ) {
         if ( key === "w" ) return palette_button.dispatchEvent( new PointerEvent( "pointerdown", { bubbles: true } ) );
 
     }
+
+}
+
+function createAnimatedStyle () {
+
+    const [ getStrength, setStrength ] = createSignal( 0 );
+
+    const tween = new Tween()
+        .fromTo( 255, 0 )
+        .setDuration( 2000 )
+        .setEasing( easeOutExpo )
+        .setListener( setStrength );
+
+    const animateStyle = _ => tween.reset().play();
+    const getStyle = _ => {
+
+        const font_color = getStrength();
+        const background_color = 255 - getStrength();
+
+        return {
+            "color": `rgb( ${ font_color } ${ font_color } ${ font_color } / 1 )`,
+            "background-color": `rgb( ${ background_color } ${ background_color } ${ background_color } / 1 )`,
+        };
+
+    };
+
+    return [ getStyle, animateStyle ];
 
 }
 
